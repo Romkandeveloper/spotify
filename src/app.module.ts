a4317fef2af058f632fa2d585df20901f282354a
roman chaban <romanchaban9@gmail.com>
2023-05-10 14:59:28 +0300

    feat(tracks): add comment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import {Module} from "@nestjs/common";
 import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-    imports: [TrackModule]
+    imports: [
+        MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@spotify.xrkqaw5.mongodb.net/?retryWrites=true&w=majority`),
+        TrackModule
+    ]
 })
 export class AppModule {}
