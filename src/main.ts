019acdb9e637661b683a6b089a8cb6ef662e13b1
roman chaban <romanchaban9@gmail.com>
2023-05-10 16:26:06 +0300

    feat(track): add create track validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
+import {ValidationPipe} from "@nestjs/common";
 
 const start = async () => {
     try {
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
-
+        app.useGlobalPipes(new ValidationPipe());
         app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
     } catch (e) {
         console.log(e);
--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,5 +1,16 @@
+import {IsNotEmpty, Length} from "class-validator";
+
 export class CreateTrackDto {
+
+    @IsNotEmpty()
+    @Length(4)
     readonly name;
+
+    @IsNotEmpty()
+    @Length(4)
     readonly artist;
+
+    @IsNotEmpty()
+    @Length(4)
     readonly text;
 }
